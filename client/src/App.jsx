33e3434f149f3ea8ec9fffc715049a054c6aef35
Ayush <ayushmehrotra099@gmail.com>
2023-10-30 13:47:54 +0530

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import TextEditor from "./components/TextEditor";
+
+function App() {
+  return (
+    <>
+      <TextEditor />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/TextEditor.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+
+const TextEditor = () => {
+  useEffect(() => {
+    new Quill("#container", { theme: "snow" });
+  }, []);
+
+  return <div id="container"></div>;
+};
+
+export default TextEditor;
