218e3bba3ff0f725c7d3d9fd95f90693bc3cea8f
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2023-10-31 14:03:07 +0530

    Client Done Mostly

--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -1,13 +1,32 @@
-import { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 
+
+
+const TOOLBAR_OPTIONS= [
+  [{header: [1,2,3,4,5,6,false] }],
+  [{font: [] }],
+  [{list: "ordered"}, {list: "bullet"}],
+  ['bold', "italic","underline"],
+  [{color: [] }, {background: [] }],
+  [{script: "sub" }, {script: "super"}],
+  [{align:[] }],
+  ['image', "blockquote", "code-block"],
+  ["clean"],
+]
+
 const TextEditor = () => {
+  const wrapperRef = useRef();
+
   useEffect(() => {
-    new Quill("#container", { theme: "snow" });
+    const editor = document.createElement("div");
+    wrapperRef.current.innerHTML = ""; // Clear any existing content
+    wrapperRef.current.append(editor);
+    new Quill(editor, { theme: "snow" , modules: {toolbar: TOOLBAR_OPTIONS}});
   }, []);
 
-  return <div id="container"></div>;
+  return <div class="container" ref={wrapperRef}></div>;
 };
 
 export default TextEditor;
